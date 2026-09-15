d25cd2b492035fcc7bc3695ec0ba4b833605ea3d
BoltZyy <vival.hari@gmail.com>
2026-09-15 18:58:03 +0700

    update deploy-commands.js (fix 100 words limit-myself)

--- deploy-commands.js
+++ deploy-commands.js
@@ -666,7 +666,7 @@ const commands = [
       },
       {
         name: 'pakai_passcard',
-        description: 'Pakai 🎟️ Advanced Passcard untuk pencairan instan tanpa penalti (maks 2x/hari, hanya utk TIER_DEPOSIT)',
+        description: 'Pakai 🎟️ Advanced Passcard untuk pencairan instan tanpa penalti (maks 2x/hari)',
         type: 5,
         required: false,
       },
